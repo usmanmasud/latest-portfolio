4b2821c3e773e71044e1c815eff49a1e4868e853
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-05-12 22:58:14 +0100

    navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,82 @@
+"use client";
+import {
+  Navbar,
+  NavBody,
+  NavItems,
+  MobileNav,
+  NavbarButton,
+  MobileNavHeader,
+  MobileNavToggle,
+  MobileNavMenu,
+} from "../components/ui/navbar-menu";
+import { useState } from "react";
+
+export function NavbarDemo() {
+  const navItems = [
+    {
+      name: "About",
+      link: "#about",
+    },
+    {
+      name: "Projects",
+      link: "#projects",
+    },
+    {
+      name: "Contact",
+      link: "#contact",
+    },
+  ];
+
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  return (
+    <div className="relative w-full">
+      <Navbar>
+        {/* Desktop Navigation */}
+        <NavBody>
+          <NavItems items={navItems} />
+          <div className="flex items-center gap-4">
+            <NavbarButton variant="primary">My Resume</NavbarButton>
+          </div>
+        </NavBody>
+
+        {/* Mobile Navigation */}
+        <MobileNav>
+          <MobileNavHeader>
+            <MobileNavToggle
+              isOpen={isMobileMenuOpen}
+              onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+            />
+          </MobileNavHeader>
+
+          <MobileNavMenu
+            isOpen={isMobileMenuOpen}
+            onClose={() => setIsMobileMenuOpen(false)}
+          >
+            {navItems.map((item, idx) => (
+              <a
+                key={`mobile-link-${idx}`}
+                href={item.link}
+                onClick={() => setIsMobileMenuOpen(false)}
+                className="relative text-neutral-600 dark:text-neutral-300"
+              >
+                <span className="block">{item.name}</span>
+              </a>
+            ))}
+            <div className="flex w-full flex-col gap-4">
+              <NavbarButton
+                onClick={() => setIsMobileMenuOpen(false)}
+                variant="primary"
+                className="w-full"
+              >
+                My Resume
+              </NavbarButton>
+            </div>
+          </MobileNavMenu>
+        </MobileNav>
+      </Navbar>
+
+      {/* Navbar */}
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@ import { StickyScrollRevealDemo } from "./components/About";
 import { BackgroundBeamsWithCollisionDemo } from "./components/BackgroundBeamsWithCollisionDemo";
 import { BackgroundGradientDemo } from "./components/Card";
 import Footer from "./components/Footer";
+import { NavbarDemo } from "./components/Navbar";
 
 export default function Home() {
   return (
     <div className="bg-gradient-to-b  text-white from-neutral-950 to-neutral-800">
+      <NavbarDemo />
       <BackgroundBeamsWithCollisionDemo />
       <StickyScrollRevealDemo />
       <BackgroundGradientDemo />
