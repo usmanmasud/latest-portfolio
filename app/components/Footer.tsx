96a1f02acc6282a342d7445fd61783e4aba73e6a
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-05-12 23:10:08 +0100

    update footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -53,7 +53,7 @@ const Footer = () => {
       </div>
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
         <p className="md:text-base text-sm md:font-normal font-light">
-          Copyright © 2024 Usman Masud Aliyu
+          Copyright © {new Date().getFullYear()} Usman Masud Aliyu
         </p>
 
         <div className="flex items-center md:gap-3 gap-6">
