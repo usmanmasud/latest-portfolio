75302173d49165522316f5fdfdfaaebf3805b8fa
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-05-12 23:29:32 +0100

    make some configurations

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -53,7 +53,10 @@ const Footer = () => {
       </div>
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
         <p className="md:text-base text-sm md:font-normal font-light">
-          Copyright © {new Date().getFullYear()} Usman Masud Aliyu
+          <a href="https://usmanmasud.vercel.app/">
+            {" "}
+            Copyright © {new Date().getFullYear()} Usman Masud Aliyu
+          </a>
         </p>
 
         <div className="flex items-center md:gap-3 gap-6">
